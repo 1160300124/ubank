da931f4e8013e2636f4a0c75bf053a89cbba78b4
huangguoqing <huangguoqing@ulaiber.com>
2017-11-24 18:44:04 +0800

    修复考勤不设置休息时段，请假后查询打卡信息的上下班时间颠倒的问题

--- ubank/src/main/webapp/js/common.js
+++ ubank/src/main/webapp/js/common.js
@@ -152,129 +152,152 @@
 		}
 	}();
 
-    var aCity = {
-        11 : "北京",
-        12 : "天津",
-        13 : "河北",
-        14 : "山西",
-        15 : "内蒙古",
-        21 : "辽宁",
-        22 : "吉林",
-        23 : "黑龙江",
-        31 : "上海",
-        32 : "江苏",
-        33 : "浙江",
-        34 : "安徽",
-        35 : "福建",
-        36 : "江西",
-        37 : "山东",
-        41 : "河南",
-        42 : "湖北",
-        43 : "湖南",
-        44 : "广东",
-        45 : "广西",
-        46 : "海南",
-        50 : "重庆",
-        51 : "四川",
-        52 : "贵州",
-        53 : "云南",
-        54 : "西藏",
-        61 : "陕西",
-        62 : "甘肃",
-        63 : "青海",
-        64 : "宁夏",
-        65 : "新疆",
-        71 : "台湾",
-        81 : "香港",
-        82 : "澳门",
-        91 : "国外"
-    };
+	var aCity = {
+			11 : "北京",
+			12 : "天津",
+			13 : "河北",
+			14 : "山西",
+			15 : "内蒙古",
+			21 : "辽宁",
+			22 : "吉林",
+			23 : "黑龙江",
+			31 : "上海",
+			32 : "江苏",
+			33 : "浙江",
+			34 : "安徽",
+			35 : "福建",
+			36 : "江西",
+			37 : "山东",
+			41 : "河南",
+			42 : "湖北",
+			43 : "湖南",
+			44 : "广东",
+			45 : "广西",
+			46 : "海南",
+			50 : "重庆",
+			51 : "四川",
+			52 : "贵州",
+			53 : "云南",
+			54 : "西藏",
+			61 : "陕西",
+			62 : "甘肃",
+			63 : "青海",
+			64 : "宁夏",
+			65 : "新疆",
+			71 : "台湾",
+			81 : "香港",
+			82 : "澳门",
+			91 : "国外"
+	};
 
 	//验证
 	window.Validate = {
-		//判断文本框不能输入特殊字符的正则表达式  能输入英文大小写字母、数字、中文 但不能输入特殊字符
-        regNumAndLetter : function (str) {
-            var reg = /^[a-zA-Z0-9\u4e00-\u9fa5]+$/;
-            if(str.match(reg) == null){
-                return false;
-            }else{
-            	return true;
-			}
-        },
-		//验证电话
-		regPhone : function (str) {
-            var reg = /^1[34578]\d{9}$/; //电话
-            if(str.match(reg) == null){
-                return false;
-            }else{
-                return true;
-            }
-        },
-		//纯数字验证
-		regNumber : function (str) {
-            var reg = /^[0-9]*$/;
-            if(str.match(reg) == null){
-                return false;
-            }else{
-                return true;
-            }
-        },
-		//数字和字母验证
-		NumberAndLetter : function (str) {
-            var reg = /^[A-Za-z0-9]+$/; //数字和字母
-            if(str.match(reg) == null){
-                return false;
-            }else{
-                return true;
-            }
-        },
-		//身份证验证
-        isCardID : function (str) {
-            // var iSum = 0;
-            // var info = "";
-            // var sBirthday = "";
-            // if (!/^\d{17}(\d|x)$/i.test(str))
-            //     return "你输入的身份证长度或格式错误";
-            // str = str.replace(/x$/i, "a");
-            // if (aCity[parseInt(str.substr(0, 2))] == null)
-            //     return "你的身份证地区非法";
-            // sBirthday = str.substr(6, 4) + "-" + Number(str.substr(10, 2)) + "-" + Number(str.substr(12, 2));
-            // var d = new Date(sBirthday.replace(/-/g, "/"));
-            // if (sBirthday != (d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()))
-            //     return "身份证上的出生日期非法";
-            // for (var i = 17; i >= 0; i--)
-            //     iSum += (Math.pow(2, i) % 11) * parseInt(str.charAt(17 - i), 11);
-            // if (iSum % 11 != 1)
-            //     return "你输入的身份证号非法";
-            // return true;
-            var reg = /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/;
-            if(str.match(reg) ==null){
-                return false;
-            }else{
-                return true;
-            }
-        },
-		//中文验证
-		regWord : function (str) {
-            var reg = /^[\u4e00-\u9fa5]+$/; //纯中文
-            if(str.match(reg) ==null){
-                return false;
-            }else{
-                return true;
-            }
-        },
-		//判断字符串是否为数字
-        isNumeric : function (str) {
-            for (var i = str.length; --i >= 0;i--) {
-                var chr = str.charAt(i);
-                if (chr < 48 || chr > 57){
-                    return false;
-                }
-            }
-            return true;
+			//判断文本框不能输入特殊字符的正则表达式  能输入英文大小写字母、数字、中文 但不能输入特殊字符
+			regNumAndLetter : function (str) {
+				var reg = /^[a-zA-Z0-9\u4e00-\u9fa5]+$/;
+				if(str.match(reg) == null){
+					return false;
+				}else{
+					return true;
+				}
+			},
+			//验证电话
+			regPhone : function (str) {
+				var reg = /^1[34578]\d{9}$/; //电话
+				if(str.match(reg) == null){
+					return false;
+				}else{
+					return true;
+				}
+			},
+			//纯数字验证
+			regNumber : function (str) {
+				var reg = /^[0-9]*$/;
+				if(str.match(reg) == null){
+					return false;
+				}else{
+					return true;
+				}
+			},
+			//数字和字母验证
+			NumberAndLetter : function (str) {
+				var reg = /^[A-Za-z0-9]+$/; //数字和字母
+				if(str.match(reg) == null){
+					return false;
+				}else{
+					return true;
+				}
+			},
+			//身份证验证
+			isCardID : function (str) {
+				// var iSum = 0;
+				// var info = "";
+				// var sBirthday = "";
+				// if (!/^\d{17}(\d|x)$/i.test(str))
+				//     return "你输入的身份证长度或格式错误";
+				// str = str.replace(/x$/i, "a");
+				// if (aCity[parseInt(str.substr(0, 2))] == null)
+				//     return "你的身份证地区非法";
+				// sBirthday = str.substr(6, 4) + "-" + Number(str.substr(10, 2)) + "-" + Number(str.substr(12, 2));
+				// var d = new Date(sBirthday.replace(/-/g, "/"));
+				// if (sBirthday != (d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()))
+				//     return "身份证上的出生日期非法";
+				// for (var i = 17; i >= 0; i--)
+				//     iSum += (Math.pow(2, i) % 11) * parseInt(str.charAt(17 - i), 11);
+				// if (iSum % 11 != 1)
+				//     return "你输入的身份证号非法";
+				// return true;
+				var reg = /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/;
+				if(str.match(reg) ==null){
+					return false;
+				}else{
+					return true;
+				}
+			},
+			//中文验证
+			regWord : function (str) {
+				var reg = /^[\u4e00-\u9fa5]+$/; //纯中文
+				if(str.match(reg) ==null){
+					return false;
+				}else{
+					return true;
+				}
+			},
+			//判断字符串是否为数字
+			isNumeric : function (str) {
+				for (var i = str.length; --i >= 0;i--) {
+					var chr = str.charAt(i);
+					if (chr < 48 || chr > 57){
+						return false;
+					}
+				}
+				return true;
 
-        }
+			}
 	}
 
+	window.mask = function() {
+		if(!$('#mask').hasClass('mask')){
+			$('body').append('<div id="mask" class="mask"><div class="progress-center"><img src="../images/loading.gif" /></div></div>');
+		}
+	}
+
+	window.unMask = function() {
+		 $('#mask').remove();
+	}
+	
 })(jQuery);
+
+$.ajaxSetup({
+	beforeSend: function(){
+		window.mask();
+	},
+	complete: function() {
+		setTimeout(function(){
+			window.unMask();
+		}, 500);
+	}
+})
+
+
 
